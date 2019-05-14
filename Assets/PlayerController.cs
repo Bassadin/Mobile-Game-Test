492a763c5e65c94e5b43ac1740de3602841f6762
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-14 10:38:42 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("Touch detected");
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            //transform.position.Set(touch.position.x, transform.position.y, transform.position.z);
+            transform.position.Set(0, 0, 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Mobile-Game-Test-Unity
