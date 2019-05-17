5c62caebcf03b9f2ec98ea1b51f831740ead8b3d
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-17 17:27:49 +0200

    Moved and updated playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float spritePixelWidth;
 
     void Start()
     {
-
+        spritePixelWidth = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -16,9 +17,10 @@ public class PlayerController : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            Vector2 touchPosition = mainCamera.ScreenToWorldPoint(touch.position);
 
             Vector3 tempVector = transform.position;
+
             //Set only X position
             tempVector.x = touchPosition.x;
             transform.position = tempVector;
