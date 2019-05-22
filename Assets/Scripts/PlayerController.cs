97885dace40332c11530d10133d14c506fc52e98
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-22 15:08:59 +0200

    Added keyboard controls and movement contraints

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,44 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
 
     float spritePixelWidth;
+    Rigidbody2D rigidbody2D;
 
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         spritePixelWidth = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 positionVector = transform.position;
+        Vector2 moveVector = Vector2.zero;
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-            //Set only X position
             float targetXCoordinate = touchPosition.x;
-            Vector2 tempVector = transform.position;
-            tempVector.x = targetXCoordinate;
-            transform.position = tempVector;
+
+            if (targetXCoordinate > transform.position.x)
+            {
+                moveVector = new Vector2(1, 0);
+            } else
+            {
+                moveVector = new Vector2(-1, 0);
+            }
+
+        } else
+        {
+            moveVector = new Vector2(Input.GetAxis("Horizontal"), 0);
+        }
+
+        if (moveVector.sqrMagnitude != 0)
+        {
+            Vector2 position = rigidbody2D.position;
+            position += moveVector * speed * Time.deltaTime;
+            rigidbody2D.MovePosition(position);
         }
     }
 }
